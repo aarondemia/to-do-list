0226bf6dc51479651aee1f82c12f700e205c7368
mikecat1024 <kensuke_seki@demia.co.jp>
2024-06-28 11:04:09 +0900

    fix: use previous

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,80 @@
-import { useState, ChangeEvent, FormEvent } from 'react'
-import './App.css'
-import Modal from "./Modal"
-import ListItem from "./ListItem"
+import { ChangeEvent, FormEvent, useState } from "react";
+import "./App.css";
+import ListItem from "./ListItem";
 
-interface ToDo {
+export interface ToDo {
   title: string;
   content: string;
 }
 
 function App() {
-
   const [toDo, setToDo] = useState<ToDo>({
     title: "",
-    content: ""
-  })
-  const [toDoList, setToDoList] = useState<ToDo[]>([{
-    title: "",
-    content: ""
-  }])
+    content: "",
+  });
+  const [toDoList, setToDoList] = useState<ToDo[]>([
+    {
+      title: "",
+      content: "",
+    },
+  ]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const { name , value } = event.target;
+    const { name, value } = event.target;
     setToDo({
       ...toDo,
-      [name]: value
-    })
-  }
+      [name]: value,
+    });
+  };
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log("Form Submitted", {toDo})
+    console.log("Form Submitted", { toDo });
     setToDo({
       title: "",
-      content: ""
-    })
-    setToDoList([...toDoList, toDo])
-  }
+      content: "",
+    });
+    setToDoList([...toDoList, toDo]);
+  };
   const handleDelete = (event: FormEvent) => {
     event.preventDefault();
-    console.log("Delete")
-    setToDoList(toDoList.slice(0, -1))
-  }
+    console.log("Delete");
+    setToDoList(toDoList.slice(0, -1));
+  };
 
   return (
     <>
-     <form onSubmit={handleSubmit}>
-      <label>
-        To Do:
-        <input 
-        required
-        type="text"
-        name="title" 
-        value={toDo.title}
-        onChange={handleChange}
-        />
-        <input 
-        required
-        type="text"
-        name="content" 
-        value={toDo.content}
-        onChange={handleChange}
-        />
-      </label>
-      <button type="submit">Submit</button>
-     </form>
+      <form onSubmit={handleSubmit}>
+        <label>
+          To Do:
+          <input
+            required
+            type="text"
+            name="title"
+            value={toDo.title}
+            onChange={handleChange}
+          />
+          <input
+            required
+            type="text"
+            name="content"
+            value={toDo.content}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
 
-      
       {toDoList.map((toDoListItem) => (
-         <ListItem  
-         key={toDoListItem.title + toDoListItem.content} 
-         toDo={toDoListItem} 
-         toDoList={toDoList} setToDoList={setToDoList} />
+        <ListItem
+          key={toDoListItem.title + toDoListItem.content}
+          toDo={toDoListItem}
+          toDoList={toDoList}
+          setToDoList={setToDoList}
+        />
       ))}
-
     </>
-
-  )
-
+  );
 }
 
-export default App
+export default App;
--- src/ListItem.tsx
+++ src/ListItem.tsx
@@ -1,38 +1,39 @@
-import React, {useState, ChangeEvent} from "react"
-import Modal from "./Modal"
+import { Dispatch, useState } from "react";
+import { ToDo } from "./App";
+import Modal from "./Modal";
 
 interface Props {
-    toDo: {title: string, content: string};
-    toDoList: {title: string, content: string}[];
-    setToDoList: (toDoList: 
-        {title: string, content: string}[]
-    ) => void;
+  toDo: { title: string; content: string };
+  toDoList: { title: string; content: string }[];
+  setToDoList: Dispatch<React.SetStateAction<ToDo[]>>;
+  //   (toDoList: { title: string; content: string }[]) => void;
 }
 
-
 function ListItem(props: Props) {
-    const [showModal, setShowModal] = useState(false)
-    const [toDo, setToDo] = useState(props.toDo)
-    const [toDoList, setToDoList] = useState(props.toDoList)
+  const [showModal, setShowModal] = useState(false);
+  const [toDo, setToDo] = useState(props.toDo);
+  const [toDoList, setToDoList] = useState(props.toDoList);
 
+  function deleteItem() {
+    console.log(toDo);
+    console.log(toDoList);
+    console.log(toDoList.filter((item) => item.title !== toDo.title));
+    props.setToDoList((previous) =>
+      previous.filter((item) => item.title !== toDo.title)
+    );
+    console.log(toDoList);
+  }
 
-    function deleteItem() {
-        console.log(toDo)
-        console.log(toDoList)
-        console.log(toDoList.filter(item => item.title !== toDo.title))
-        props.setToDoList(toDoList.filter(item => item.title !== toDo.title))
-        console.log(toDoList)
-    }
-
-    return (
-        <div>
-            <p>{props.toDo.title}</p>
-            <button onClick={() => setShowModal(true)}>EDIT</button>
-            <button onClick={deleteItem}>DELETE</button>
-        </div>
-        // {showModal && <Modal toDo: {props.toDo} setShowModal={setShowModal}/>}
-
-    )
+  return (
+    <>
+      <div>
+        <p>{props.toDo.title}</p>
+        <button onClick={() => setShowModal(true)}>EDIT</button>
+        <button onClick={deleteItem}>DELETE</button>
+      </div>
+      {showModal && <Modal toDo={props.toDo} setShowModal={setShowModal} />}
+    </>
+  );
 }
 
 export default ListItem;
